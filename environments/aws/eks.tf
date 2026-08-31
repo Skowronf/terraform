c7a940a725a75bba185ff560d2e22ba6201c8c7e
Skowronf <skowron.f.j@gmail.com>
2026-08-31 13:39:14 +0000

    add pod identity agent as an addon to eks cluster

--- environments/aws/eks.tf
+++ environments/aws/eks.tf
@@ -15,3 +15,9 @@ resource "aws_eks_cluster" "main" {
     Name = "petclinic-eks"
   }
 }
+
+# Add pod identity agent add-on to the EKS cluster.
+resource "aws_eks_addon" "pod_identity_agent" {
+  cluster_name  = aws_eks_cluster.main.name
+  addon_name    = "eks-pod-identity-agent"
+}
